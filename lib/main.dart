5d499803af7659e794b96c2230462aa65bf176c7
hamid <hamidmsukardi@gmail.com>
2020-06-24 14:07:48 +0700

    Box Decoration

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("Belajar Container")),
-        body: Container(color: Colors.lightBlue, margin: EdgeInsets.all(10),),
+        body: Container(
+          color: Colors.lightBlue,
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.only(bottom: 40, top: 20),
+          child: Container(
+            // color: Colors.blue,
+            margin: EdgeInsets.fromLTRB(10, 15, 20, 25),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: <Color>[
+                  Colors.lightBlueAccent,
+                  Colors.blue,
+                ])),
+          ),
+        ),
       ),
     );
   }
